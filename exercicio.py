d0b28688f9dead0f8252c02b5f63037dfc12d9dd
Vito Virito <vitor.landin@aluno.senai.br>
2026-02-11 16:44:18 -0300

    bunny
    bunny

--- exercicio.py
+++ exercicio.py
@@ -1,1 +1,27 @@
 import flet as ft
+
+def main(page: ft.Page):
+    def mostrar_mensagem(e):
+        page.add(ft.Text("Ai."))
+        
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(
+        ft.Text(
+            "*Barulho de Coelho*",
+            weight=ft.FontWeight.W_600,
+            ),
+        ft.Image(
+            src="images/link.png",
+            height=350,
+            visible=True
+        ),
+        ft.Button(
+            content="Bater no coelho",
+            on_click=mostrar_mensagem,
+            bgcolor="#1c0138"
+            
+        )
+    )
+
+ft.app(main)
